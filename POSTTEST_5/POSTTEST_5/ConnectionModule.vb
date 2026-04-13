ae8f792caaf86a744cbe0767c0a420be5e0880ce
Adityamr2005 <adityamahyudiramadhan403@gmail.com>
2026-04-13 22:19:36 +0800

    Delete POSTTEST_5 directory

--- POSTTEST_5/POSTTEST_5/ConnectionModule.vb
+++
@@ -1,7 +0,0 @@
-﻿Imports MySqlConnector
-Module ConnectionModule
-    Public ReadOnly ConnectionString As String = "Server=localhost;Port=3306;Database=db_acecraft;User ID=root;Password=;"
-    Public Function GetConnection() As MySqlConnection
-        Return New MySqlConnection(ConnectionString)
-    End Function
-End Module
